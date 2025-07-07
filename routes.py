b185b5040e68e88b012596e4cd4cc836cb142168
RISHABH72git <rishabhmourya72@gamil.com>
2025-07-07 15:17:24 +0530

    reuse uniqueId if same url short link created reuse this

--- routes.py
+++ routes.py
@@ -12,23 +12,31 @@ router = APIRouter()
 
 
 @router.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    host = str(request.base_url)
+    return BaseResponse(message="Apis list",
+                        data={"url_short": f"{host}url/short?long_url=https://www.google.com&exp_sec=500",
+                              "get_long_url": f"{host}wlakUHKh"})
 
 
 @router.get("/url/short")
 async def url_short(long_url: str, request: Request, exp_sec: Optional[int] = None):
-    # Random uppercase letter
-    random_chars = ''.join(random.choices(string.ascii_uppercase, k=3))
-    # Random lowercase letter
-    random_lower = ''.join(random.choices(string.ascii_lowercase, k=3))
-    url_id = f"{long_url[-1]}{random_chars}{random_lower}{long_url[0]}"
     redis_client = get_redis_client()
-    if not await redis_client.exists(url_id):
-        if exp_sec:
-            await redis_client.set(url_id, long_url, ex=exp_sec)
-        else:
-            await redis_client.set(url_id, long_url)
+    if await redis_client.exists(long_url):
+        url_id = await redis_client.get(long_url)
+    else:
+        # Random uppercase letter
+        random_chars = ''.join(random.choices(string.ascii_uppercase, k=3))
+        # Random lowercase letter
+        random_lower = ''.join(random.choices(string.ascii_lowercase, k=3))
+        url_id = f"{long_url[-1]}{random_chars}{random_lower}{long_url[0]}"
+        redis_client = get_redis_client()
+        if not await redis_client.exists(url_id):
+            if exp_sec:
+                await redis_client.set(url_id, long_url, ex=exp_sec)
+            else:
+                await redis_client.set(url_id, long_url)
+        await redis_client.set(long_url, url_id)
 
     host = str(request.base_url)
     short_url = f"{host}{url_id}"

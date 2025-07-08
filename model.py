56d5b17da8b5f5768a2330123bf369adb1873ca6
RISHABH72git <rishabhmourya72@gamil.com>
2025-07-08 15:31:58 +0530

    create post api and add requirements.txt

--- model.py
+++ model.py
@@ -4,3 +4,6 @@ from pydantic import BaseModel, HttpUrl
 class BaseResponse(BaseModel):
     message: str
     data: dict
+
+class LongUrl(BaseModel):
+    long_url: str
--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from typing import Optional
 from fastapi import APIRouter, Request, HTTPException
 from starlette.responses import RedirectResponse, JSONResponse
 
-from model import BaseResponse
+from model import BaseResponse, LongUrl
 from redis_config import get_redis_client
 
 router = APIRouter()
@@ -21,6 +21,31 @@ async def root(request: Request):
 
 @router.get("/url/short")
 async def url_short(long_url: str, request: Request, exp_sec: Optional[int] = None):
+    redis_client = get_redis_client()
+    if await redis_client.exists(long_url):
+        url_id = await redis_client.get(long_url)
+    else:
+        # Random uppercase letter
+        random_chars = ''.join(random.choices(string.ascii_uppercase, k=3))
+        # Random lowercase letter
+        random_lower = ''.join(random.choices(string.ascii_lowercase, k=3))
+        url_id = f"{long_url[-1]}{random_chars}{random_lower}{long_url[0]}"
+        redis_client = get_redis_client()
+        if not await redis_client.exists(url_id):
+            if exp_sec:
+                await redis_client.set(url_id, long_url, ex=exp_sec)
+            else:
+                await redis_client.set(url_id, long_url)
+        await redis_client.set(long_url, url_id)
+
+    host = str(request.base_url)
+    short_url = f"{host}{url_id}"
+    return BaseResponse(message="short url created", data={"short_url": short_url, "original_url": long_url})
+
+
+@router.post("/url/short")
+async def url_short(long_url: LongUrl, request: Request, exp_sec: Optional[int] = None):
+    long_url = long_url.long_url
     redis_client = get_redis_client()
     if await redis_client.exists(long_url):
         url_id = await redis_client.get(long_url)
